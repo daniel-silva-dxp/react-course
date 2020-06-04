35ef15e6e85ce1d3fdb0533ff39aa2388ef8154c
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-04 15:59:24 -0300

    feat: criando class component e trabalhando com states

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import Social from './Social';
+import React, { Component } from 'react';
 import Equipe from './Equipe';
 
-const App = () => {
-	return (
-		<div>
-			<Social />
-			<Equipe nome="Daniel Silva" cargo="Programador" idade="32" />
-		</div>
-	);
-};
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			devs: [
+				{
+					nome: 'Daniel Silva',
+					cargo: 'Desenvolvedor Front-end',
+					idade: 32
+				},
+				{
+					nome: 'Matheus Silva',
+					cargo: 'Desenvolvedor Back-end',
+					idade: 18
+				},
+				{
+					nome: 'Henrique Lima',
+					cargo: 'Programador',
+					idade: 30
+				}
+			]
+		};
+	}
+	render() {
+		return (
+			<div>
+				{this.state.devs.map((dev) => {
+					return <Equipe nome={dev.nome} cargo={dev.cargo} idade={dev.idade} />;
+				})}
+			</div>
+		);
+	}
+}
 
 export default App;
