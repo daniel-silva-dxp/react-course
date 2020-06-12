7bd3a34917108e733f160139108a57d381361ffe
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-12 13:41:24 -0300

    feat: aplicando estilo ao css

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './index.css';
 
 const Button = ({ children, handleClick }) => {
-	return <button onClick={handleClick}>{children}</button>;
+	return (
+		<button onClick={handleClick} className="btn btn-medium btn-color">
+			{children}
+		</button>
+	);
 };
 
 Button.propTypes = {
 	handleClick: PropTypes.func.isRequired
-}
+};
 
 export default Button;
