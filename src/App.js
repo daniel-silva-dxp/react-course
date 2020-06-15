e75598775cf77a908eaf10c47fa27ebff31719d7
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-15 09:05:02 -0300

    feat: limpando APP.js para novo treinamento - consumindo API

--- src/App.js
+++ src/App.js
@@ -1,57 +1,17 @@
 import React, { Component } from 'react';
-import Display from './components/display-timer';
 
 import './app.css';
 
 class App extends Component {
 	constructor() {
 		super();
-		this.state = {
-			timer: '00:00:00',
-			workHours: 0
-		};
-		this.timer = null;
-		this.play = this.play.bind(this);
-		this.pause = this.pause.bind(this);
-		this.reset = this.reset.bind(this);
-	}
-	timeFormat(counter) {
-		let display = new Date(counter * 1000);
-		return display.toLocaleTimeString('pt-BR', {
-			hour12: false,
-			timeZone: 'UTC'
-		});
-	}
-	play() {
-		this.timer = setInterval(() => {
-			this.setState({
-				workHours: this.state.workHours + 1,
-				timer: this.timeFormat(this.state.workHours)
-			});
-		}, 1000);
-	}
-	pause() {
-		clearInterval(this.timer);
-	}
-	reset() {
-		clearInterval(this.timer);
-		this.setState({
-			timer: '00:00:00',
-			workHours: 0
-		});
+		this.state = {};
 	}
 
 	render() {
 		return (
 			<div className="container">
-				<div className="content text-align">
-					<Display
-						timer={this.state.timer}
-						handlePlay={this.play}
-						handlePuse={this.pause}
-						handleReset={this.reset}
-					/>
-				</div>
+				<h1>API</h1>
 			</div>
 		);
 	}
