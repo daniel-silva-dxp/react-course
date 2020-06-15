f504f671fc359975bba5b2aa55de8be53db4d1aa
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-15 11:08:27 -0300

    feat: implementando fetch da API e atribuindo response ao state nutri

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 
 import './app.css';
+import Nutri from './components/nutri';
 
 class App extends Component {
 	constructor() {
 		super();
-		this.state = {};
+		this.state = {
+			nutri: []
+		};
+	}
+
+	componentDidMount() {
+		const url = 'http://sujeitoprogramador.com/rn-api/?api=posts';
+		fetch(url).then((post) => post.json()).then((post) => {
+			this.setState({
+				nutri: post
+			});
+		});
 	}
 
 	render() {
 		return (
 			<div className="container">
-				<h1>API</h1>
+				<header>
+					<strong>React Nitri</strong>
+				</header>
+				<Nutri nutri={this.state.nutri} />
 			</div>
 		);
 	}
