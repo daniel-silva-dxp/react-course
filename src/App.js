b5bfd9a3960e3426960ca5ee6b78f466c6e2ef58
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-15 17:39:11 -0300

    feat: limpando App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React, { Component } from 'react';
 
 import './app.css';
-import Nutri from './components/nutri';
 
 class App extends Component {
 	constructor() {
 		super();
-		this.state = {
-			nutri: []
-		};
-	}
-
-	componentDidMount() {
-		const url = 'http://sujeitoprogramador.com/rn-api/?api=posts';
-		fetch(url).then((post) => post.json()).then((post) => {
-			this.setState({
-				nutri: post
-			});
-		});
+		this.state = {};
 	}
 
 	render() {
 		return (
 			<div className="container">
-				<header>
-					<strong>React Nitri</strong>
-				</header>
-				<Nutri nutri={this.state.nutri} />
+				<h1>TODO LIST</h1>
 			</div>
 		);
 	}
